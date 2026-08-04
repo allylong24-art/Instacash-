bdb7e75857d38654aedb0ed61b203ca39d6bced7
allylong24-art <allylong24@icloud.com>
2026-08-04 16:24:14 -0400

    Create Color.kt

---
+++ app/src/main/java/com/example/data/entity/app/src/main/java/com/example/data/entity/Color.kt
@@ -0,0 +1,20 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BentoBluePrimary = Color(0xFF0061A4)
+val BentoBlueDark = Color(0xFF00325B)
+val BentoBlueContainer = Color(0xFFD3E4FF)
+val BentoOnBlueContainer = Color(0xFF001C38)
+val BentoPurpleContainer = Color(0xFFF1DFFF)
+val BentoOnPurpleContainer = Color(0xFF21103E)
+val BentoGreenContainer = Color(0xFFDBF0DA)
+val BentoOnGreenContainer = Color(0xFF116D27)
+val BentoBackground = Color(0xFFFBFCFF)
+val BentoSurface = Color(0xFFFFFFFF)
+
+val AmberAccent = Color(0xFF0061A4)
+val EmeraldPrimary = Color(0xFF0061A4)
+val EmeraldDark = Color(0xFF00325B)
+val DarkBackground = Color(0xFF001E2F)
+val DarkSurface = Color(0xFF191C1E)
