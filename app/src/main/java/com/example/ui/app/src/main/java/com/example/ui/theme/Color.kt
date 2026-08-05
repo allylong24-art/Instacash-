7ee595c5a46a7158da52798adb84d2afddee1d6a
allylong24-art <allylong24@icloud.com>
2026-08-05 17:30:50 -0400

    Create Color.kt

---
+++ app/src/main/java/com/example/ui/app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Emerald80 = Color(0xFF80F0AA)
+val EmeraldGrey80 = Color(0xFFB9CCBC)
+val Mint80 = Color(0xFFA0E8D0)
+
+val Emerald40 = Color(0xFF006C3B)
+val EmeraldGrey40 = Color(0xFF4C6353)
+val Mint40 = Color(0xFF006B54)
+
+val InstantGold = Color(0xFFFFB800)
+val DarkBackground = Color(0xFF101412)
+val DarkSurface = Color(0xFF1B211E)
+val CardBorder = Color(0xFF2E3833)
